a5a669b1902f65f0e58daf8ce31fe9c379d15550
Avoloos <avoloos@hotmail.com>
2014-12-24 11:00:18 +0100

    Warlock: Upped the other versions too.

--- Warlock/Avoloos_Warlock_BaseRotation.cs
+++ Warlock/Avoloos_Warlock_BaseRotation.cs
@@ -14,9 +14,9 @@ namespace Avoloos
     /// </summary>
     public static class RotationVersion
     {
-        public const string WarlockIcyVeinsAffliction = "1.1.1";
+        public const string WarlockIcyVeinsAffliction = "1.1.2";
         public const string WarlockIcyVeinsDestruction = "1.1.2";
-        public const string WarlockIcyVeinsDemonology = "1.1.2";
+        public const string WarlockIcyVeinsDemonology = "1.1.3";
     }
 
     /// <summary>
